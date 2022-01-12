d21534588b6b6be5eb13f7de7e1b2bd68c8d3a10
Anonymous Dev <96826139+freecontenttest@users.noreply.github.com>
2022-01-12 16:05:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -364,11 +364,13 @@ bot.command(['short_to_droplink', 'short_to_shorturllink'], async (ctx) => {
         const isDropLink = ctx.message.text.includes('droplink')
         const token = isDropLink ? process.env.DROPLINK_API_TOKEN : process.env.SHORTURLLINK_API_TOKEN;
         const endpoint = isDropLink ? 'droplink.co' : 'shorturllink.in';
+        const finalURL = isDropLink ? `https://droplink.co/api?api=${token}&url=${linkToShort}` : `https://shorturllink.in/api?api=${token}&url=${linkToShort}`;
+        console.log('finalURL====', finalURL)
 //         const apiURL = `https://${endpoint}/api?api=${token}&url=${linkToShort}`;
 //         console.log('apiurl===', apiURL);
         
         try {
-            const response = await axios.get(`https://${endpoint}/api?api=${token}&url=${linkToShort}`);
+            const response = await axios.get(finalURL);
             if (response.data.status === 'success') {
                 db.createData({ body: [response.data.shortenedUrl, URL, uniqID, video_name, video_size, video_duration] })
                     .then(async (res) => {
