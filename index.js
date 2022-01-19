7634fac11c2a67a4e8d141c997428911212ca2e4
Anonymous Dev <96826139+freecontenttest@users.noreply.github.com>
2022-01-19 14:51:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -337,9 +337,13 @@ bot.command('hello', async (ctx) => {
         api: '8877d9afc0127597a60bc91b53d0df5e66691582',
         url: url
     };
-    const APIURL = `https://shorturllink.in/api?api=${params.api}&url=${params.url}&alias=CustomAlias`;
+    const APIURL = `https://shorturllink.in/api?api=${params.api}&url=${params.url}`;
     try {
-        const response = await https.get(APIURL);
+        const response = await axios.get(APIURL, {
+            headers: {
+                Cookie: "csrfToken=792194576d97e6f23980988fd35b69ea7ea61aaa846ada096c057c4c8bb6f56f922c9d9840b01db208526500f272078eb6286b52d740a530d55c250aa2484882; AppSession=531a50219433d9802f91806ef0b18ed9"
+            }
+        });
         console.log('res---hrllo', response)
     }
     catch(err) {
