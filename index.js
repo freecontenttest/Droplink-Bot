8576842897f3ad20896dcd29444567374bf22436
Anonymous Dev <96826139+freecontenttest@users.noreply.github.com>
2022-03-01 15:28:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const { Client } = require('pg');
 const client = new Client({
     connectionString: process.env.DATABASE_URL,
     ssl: {
-        rejectUnauthorized: true,
+        rejectUnauthorized: false,
     }
 });
 client.connect();
